783c97e78d5f477968f1ec4038795cf12c8fc6d3
OliveiraNt <solon_oliveira@live.com>
2017-05-01 15:49:59 -0300

    OpenGL and Processing folders added

--- OpenGL/Prism.py
+++ OpenGL/Prism.py
@@ -1,42 +1,62 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
-import math
+from  math import sin, cos, pi
+import sys
 
 
 colors = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
 
-def pyramid(n, r):
-	PI = 3.1415926535897932384626433832795
+def prism(n, r, l):
 	teta = 0
 	x = r
 	y, z = 0, 0
-	c = 0
+	z2 = l
 
-	glBegin(GL_TRIANGLE_FAN)
-
-	glColor3fv(colors[c])
-	glVertex3f(0, r, 0)
+	glBegin(GL_QUAD_STRIP)
 
 	for v in range(n+1):
-		glColor3fv(colors[c])
-		x2 = (x * math.cos(teta) - y * math.sin(teta))
-		y2 = (x * math.sin(teta) + y * math.cos(teta))
+		glColor3fv(colors[v%8])
+		x2 = (x * cos(teta) - y * sin(teta))
+		y2 = (x * sin(teta) + y * cos(teta))
 		glVertex3f(x2, z, y2)
-		teta += (360/n)*PI/180
-		c += 1
+		glVertex3f(x2, z2, y2)
+		teta += (2*pi)/n
+	glEnd()
+
+	glBegin(GL_POLYGON)
+
+	teta = 0
+	for v in range(n):
+		glColor3fv(colors[v%8])
+		x2 = (x * cos(teta) - y * sin(teta))
+		y2 = (x * sin(teta) + y * cos(teta))
+		glVertex3f(x2, z, y2)
+		teta += (2*pi)/n
+	glEnd()
+
+	glBegin(GL_POLYGON)
+
+	teta = 0
+	for v in range(n):
+		glColor3fv(colors[v%8])
+		x2 = (x * cos(teta) - y * sin(teta))
+		y2 = (x * sin(teta) + y * cos(teta))
+		glVertex3f(x2, z2, y2)
+		teta += (2*pi)/n
 	glEnd()
 	
 
 def abacaxi():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glRotatef(2,1,3,0)
-    pyramid(6, 2)
+    prism(3, 2, 3)
     glutSwapBuffers()
  
 def timer(i):
     glutPostRedisplay()
     glutTimerFunc(50,timer,1)
+
 
 # PROGRAMA PRINCIPAL
 glutInit(sys.argv)
@@ -52,5 +72,3 @@ glTranslatef(0.0,0.0,-10)
 glRotatef(45,1,1,1)
 glutTimerFunc(50,timer,1)
 glutMainLoop()
-
-
